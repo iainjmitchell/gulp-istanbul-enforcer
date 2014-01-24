3adb0bd2de7117d71a330b95a711f846cc9f5802
iainjmitchell <iainjmitchell@gmail.com>
2014-01-24 16:10:41 +0000

    adding initial files et al

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+	mocha = require('gulp-mocha'),
+	jshint = require('gulp-jshint'),
+	git = require('gulp-git');
+
+gulp.task('mocha', function () {
+	var mochaRun = mocha({
+			reporter: 'nyan',
+			ui: 'tdd'
+		});
+
+	return gulp
+		.src('test/**/*.js')
+		.pipe(mochaRun);
+});
+
+gulp.task('jshint', function(){
+	return gulp
+		.src(['lib/**/*.js', 'test/**/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', ['jshint', 'mocha']);
+
+gulp.task('push', ['test'], function(){
+	var commitMessage = gulp.env.message || 'no commit message';
+	console.log('Tests passed! Pushing code...');
+	return gulp
+		.src('./.')
+		.pipe(git.add())
+		.pipe(git.commit(commitMessage))
+		.pipe(git.push());
+});
+
+gulp.task('default', function(){
+	gulp.run('push');
+});
