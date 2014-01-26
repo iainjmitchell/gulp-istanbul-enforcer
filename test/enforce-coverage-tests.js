78a0235a9a6a8dfea8c001fb6747eb1e14fb50a1
iainjmitchell <iainjmitchell@gmail.com>
2014-01-26 18:11:34 +0000

    creating a check-coverage command

--- test/enforce-coverage-tests.js
+++ test/enforce-coverage-tests.js
@@ -1,7 +1,17 @@
+var rewire = require('rewire'),
+	CoverageEnforcer = rewire('../lib/gulp-istanbul-enforcer');
+
 require('chai').should();
 
 describe('When coverage is enforced', function(){
-	it('true', function(){
-		true.should.equal(true);
+	it('Then check-coverage istanbul command created', function(){
+		var mockCommandFactory = {
+				create : function(commandType){
+					this.createType = commandType;
+				}
+			};
+		CoverageEnforcer.__set__('commandFactory', mockCommandFactory);
+		CoverageEnforcer();
+		mockCommandFactory.createType.should.equal('check-coverage');
 	});
 });
