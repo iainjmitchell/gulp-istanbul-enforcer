e93994430e599d9207738a210baff3f90632785d
iainjmitchell <iainjmitchell@gmail.com>
2014-01-26 18:35:36 +0000

    statements included in command run

--- test/enforce-coverage-tests.js
+++ test/enforce-coverage-tests.js
@@ -4,10 +4,17 @@ var rewire = require('rewire'),
 require('chai').should();
 
 describe('When coverage is enforced', function(){
+	var MockCommand = function(){
+			this.run = function(runArguments){
+				this.runArguments = runArguments;
+			};
+		};
+
 	it('Then check-coverage istanbul command created', function(){
 		var mockCommandFactory = {
 				create : function(commandType){
 					this.createType = commandType;
+					return new MockCommand();
 				}
 			};
 		CoverageEnforcer.__set__('commandFactory', mockCommandFactory);
@@ -15,5 +22,21 @@ describe('When coverage is enforced', function(){
 		stream.write();
 		stream.end();
 		mockCommandFactory.createType.should.equal('check-coverage');
+	});
+
+	it('Then check-coverage command is started with statements level in arguments', function(){
+		var FakeCommandFactory = function(command){
+				this.create = function(){
+					return command;
+				};
+			},
+			mockCommand = new MockCommand();
+		CoverageEnforcer.__set__('commandFactory', new FakeCommandFactory(mockCommand));
+		var stream = CoverageEnforcer({
+			statements : 90
+		});
+		stream.write();
+		stream.end();
+		mockCommand.runArguments.should.include('--statements=90');
 	});
 });
