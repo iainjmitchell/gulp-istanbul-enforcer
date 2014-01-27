40ed700352b732c502538e11f246327fb0a829fe
iainjmitchell <iainjmitchell@gmail.com>
2014-01-27 19:08:56 +0000

    adding coverage directory

--- test/enforce-coverage-tests.js
+++ test/enforce-coverage-tests.js
@@ -77,6 +77,18 @@ describe('When coverage is enforced', function(){
 		mockCommand.runArguments.should.include('--functions=' + functions);
 	});
 
+	it('Then check-coverage command is started with coverage directory in arguments', function(){
+		var mockCommand = new MockCommand(),
+			coverageDirectory = 'a ranndom directory ' + Math.random();
+		CoverageEnforcer.__set__('commandFactory', new FakeCommandFactory(mockCommand));
+		var stream = CoverageEnforcer({
+			coverageDirectory : coverageDirectory
+		});
+		stream.write();
+		stream.end();
+		mockCommand.runArguments.should.include('--dir=' + coverageDirectory);
+	});
+
 	it('Then it should call the callback for the flush method', function (done) {
 		var stream = CoverageEnforcer({});
 		stream._flush(function () {
